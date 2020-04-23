76dd7464e83f6dff7ceacbd999610fdc5140db9f
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-04-23 21:03:27 +0300

    Use file_write

--- sandbox/main.c
+++ sandbox/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <utils/utils.h>
 
 int main()
 {
 	printf("Hello world.\n");
+	char helloworld[] = "Hello world.";
+	file_write("test", (unsigned char *)helloworld, strlen(helloworld));
 	return 0;
 }
---
+++ src/utils/utils.h
@@ -0,0 +1,8 @@
+#ifndef MOD_UTILS_H
+#define MOD_UTILS_H
+
+#include "file/file.h"
+#include "file/file_write.c"
+#include "file/file_read.c"
+
+#endif
