c1ad45cd9463f0ad6e0cfe5502a54587b1287bde
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-05-04 17:36:00 +0300

    dv2: rename arguments

--- src/utils/maths/vecs.h
+++ src/utils/maths/vecs.h
@@ -138,22 +138,22 @@ typedef union dvec2 {
 #endif
 	FLOAT d[2];
 } dvec2;
-void dvec2s_set(dvec2 *vec, FLOAT v1, FLOAT v2);
-void dvec2v_set(dvec2 *vec, const dvec2 *vec2);
-void dvec2_set(dvec2 *vec, FLOAT v[2]);
-void dvec2s_mul(dvec2 *vec, FLOAT v1, FLOAT v2);
-void dvec2_mul(dvec2 *vec, FLOAT v[2]);
-void dvec2u_mul(dvec2 *vec, FLOAT v);
-void dvec2v_mul(dvec2 *vec, const dvec2 *vec2);
-void dvec2s_add(dvec2 *vec, FLOAT v1, FLOAT v2);
-void dvec2_add(dvec2 *vec, FLOAT v[2]);
-void dvec2u_add(dvec2 *vec, FLOAT v);
-void dvec2v_add(dvec2 *vec, const dvec2 *vec2);
-void dvec2s_sub(dvec2 *vec, FLOAT v1, FLOAT v2);
-void dvec2_sub(dvec2 *vec, FLOAT v[2]);
-void dvec2u_sub(dvec2 *vec, FLOAT v);
-void dvec2v_sub(dvec2 *vec, const dvec2 *vec2);
-FLOAT dvec2_mag(const dvec2 *vec);
+void dvec2s_set(dvec2 *vec0, FLOAT v0, FLOAT v1);
+void dvec2v_set(dvec2 *vec0, const dvec2 *vec1);
+void dvec2_set(dvec2 *vec0, FLOAT v1[2]);
+void dvec2s_mul(dvec2 *vec0, FLOAT v0, FLOAT v1);
+void dvec2_mul(dvec2 *vec0, FLOAT v1[2]);
+void dvec2u_mul(dvec2 *vec0, FLOAT v1);
+void dvec2v_mul(dvec2 *vec0, const dvec2 *vec1);
+void dvec2s_add(dvec2 *vec0, FLOAT v0, FLOAT v1);
+void dvec2_add(dvec2 *vec0, FLOAT v1[2]);
+void dvec2u_add(dvec2 *vec0, FLOAT v1);
+void dvec2v_add(dvec2 *vec0, const dvec2 *vec1);
+void dvec2s_sub(dvec2 *vec0, FLOAT v0, FLOAT v1);
+void dvec2_sub(dvec2 *vec0, FLOAT v1[2]);
+void dvec2u_sub(dvec2 *vec0, FLOAT v1);
+void dvec2v_sub(dvec2 *vec0, const dvec2 *vec1);
+FLOAT dvec2_mag(const dvec2 *vec0);
 
 typedef union dvec3 {
 #if MATH_USE_STRUCTS
--- src/utils/maths/vecs/dv2.c
+++ src/utils/maths/vecs/dv2.c
@@ -3,105 +3,105 @@
 
 #include "../vecs.h"
 
-void dvec2s_set(dvec2 *vec, FLOAT v1, FLOAT v2)
+void dvec2s_set(dvec2 *vec0, FLOAT v0, FLOAT v1)
 {
-	assert(vec);
-	vec->d[0] = v1;
-	vec->d[1] = v2;
+	assert(vec0);
+	vec0->d[0] = v0;
+	vec0->d[1] = v1;
 }
-void dvec2v_set(dvec2 *vec, const dvec2 *vec2)
+void dvec2v_set(dvec2 *vec0, const dvec2 *vec1)
 {
-	assert(vec);
-	assert(vec2);
-	memcpy(vec, vec2, sizeof(*vec));
+	assert(vec0);
+	assert(vec1);
+	memcpy(vec0, vec1, sizeof(dvec2));
 }
-void dvec2_set(dvec2 *vec, FLOAT v[2])
+void dvec2_set(dvec2 *vec0, FLOAT v1[2])
 {
-	assert(vec);
-	memcpy(vec->d, v, 2 * sizeof(FLOAT));
-}
-
-void dvec2s_mul(dvec2 *vec, FLOAT v1, FLOAT v2)
-{
-	assert(vec);
-	vec->d[0] *= v1;
-	vec->d[1] *= v2;
-}
-void dvec2_mul(dvec2 *vec, FLOAT v[2])
-{
-	assert(vec);
-	vec->d[0] *= v[0];
-	vec->d[1] *= v[1];
-}
-void dvec2u_mul(dvec2 *vec, FLOAT v)
-{
-	assert(vec);
-	vec->d[0] *= v;
-	vec->d[1] *= v;
-}
-void dvec2v_mul(dvec2 *vec, const dvec2 *vec2)
-{
-	assert(vec);
-	assert(vec2);
-	vec->d[0] *= vec2->d[0];
-	vec->d[1] *= vec2->d[1];
+	assert(vec0);
+	memcpy(vec0->d, v1, 2 * sizeof(FLOAT));
 }
 
-void dvec2s_add(dvec2 *vec, FLOAT v1, FLOAT v2)
+void dvec2s_mul(dvec2 *vec0, FLOAT v0, FLOAT v1)
 {
-	assert(vec);
-	vec->d[0] += v1;
-	vec->d[1] += v2;
+	assert(vec0);
+	vec0->d[0] *= v0;
+	vec0->d[1] *= v1;
 }
-void dvec2_add(dvec2 *vec, FLOAT v[2])
+void dvec2_mul(dvec2 *vec0, FLOAT v1[2])
 {
-	assert(vec);
-	vec->d[0] += v[0];
-	vec->d[1] += v[1];
+	assert(vec0);
+	vec0->d[0] *= v1[0];
+	vec0->d[1] *= v1[1];
 }
-void dvec2u_add(dvec2 *vec, FLOAT v)
+void dvec2u_mul(dvec2 *vec0, FLOAT v1)
 {
-	assert(vec);
-	vec->d[0] += v;
-	vec->d[1] += v;
+	assert(vec0);
+	vec0->d[0] *= v1;
+	vec0->d[1] *= v1;
 }
-void dvec2v_add(dvec2 *vec, const dvec2 *vec2)
+void dvec2v_mul(dvec2 *vec0, const dvec2 *vec1)
 {
-	assert(vec);
-	assert(vec2);
-	vec->d[0] += vec2->d[0];
-	vec->d[1] += vec2->d[1];
-}
-
-void dvec2s_sub(dvec2 *vec, FLOAT v1, FLOAT v2)
-{
-	assert(vec);
-	vec->d[0] -= v1;
-	vec->d[1] -= v2;
-}
-void dvec2_sub(dvec2 *vec, FLOAT v[2])
-{
-	assert(vec);
-	vec->d[0] -= v[0];
-	vec->d[1] -= v[1];
-}
-void dvec2u_sub(dvec2 *vec, FLOAT v)
-{
-	assert(vec);
-	vec->d[0] -= v;
-	vec->d[1] -= v;
-}
-void dvec2v_sub(dvec2 *vec, const dvec2 *vec2)
-{
-	assert(vec);
-	assert(vec2);
-	vec->d[0] -= vec2->d[0];
-	vec->d[1] -= vec2->d[1];
-	vec->d[2] -= vec2->d[2];
+	assert(vec0);
+	assert(vec1);
+	vec0->d[0] *= vec1->d[0];
+	vec0->d[1] *= vec1->d[1];
 }
 
-FLOAT dvec2_mag(const dvec2 *vec)
+void dvec2s_add(dvec2 *vec0, FLOAT v0, FLOAT v1)
 {
-	return sqrt(pow(vec->d[0], 2) + pow(vec->d[1], 2));
+	assert(vec0);
+	vec0->d[0] += v0;
+	vec0->d[1] += v1;
+}
+void dvec2_add(dvec2 *vec0, FLOAT v1[2])
+{
+	assert(vec0);
+	vec0->d[0] += v1[0];
+	vec0->d[1] += v1[1];
+}
+void dvec2u_add(dvec2 *vec0, FLOAT v1)
+{
+	assert(vec0);
+	vec0->d[0] += v1;
+	vec0->d[1] += v1;
+}
+void dvec2v_add(dvec2 *vec0, const dvec2 *vec1)
+{
+	assert(vec0);
+	assert(vec1);
+	vec0->d[0] += vec1->d[0];
+	vec0->d[1] += vec1->d[1];
+}
+
+void dvec2s_sub(dvec2 *vec0, FLOAT v0, FLOAT v1)
+{
+	assert(vec0);
+	vec0->d[0] -= v0;
+	vec0->d[1] -= v1;
+}
+void dvec2_sub(dvec2 *vec0, FLOAT v1[2])
+{
+	assert(vec0);
+	vec0->d[0] -= v1[0];
+	vec0->d[1] -= v1[1];
+}
+void dvec2u_sub(dvec2 *vec0, FLOAT v1)
+{
+	assert(vec0);
+	vec0->d[0] -= v1;
+	vec0->d[1] -= v1;
+}
+void dvec2v_sub(dvec2 *vec0, const dvec2 *vec1)
+{
+	assert(vec0);
+	assert(vec1);
+	vec0->d[0] -= vec1->d[0];
+	vec0->d[1] -= vec1->d[1];
+	vec0->d[2] -= vec1->d[2];
+}
+
+FLOAT dvec2_mag(const dvec2 *vec0)
+{
+	return sqrt(pow(vec0->d[0], 2) + pow(vec0->d[1], 2));
 }
 #endif
