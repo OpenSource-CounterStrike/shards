00cfb9f295fd099fca3eb8071034497a244a6e26
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-05-04 17:29:39 +0300

    iv4: rename arguments

--- src/utils/maths/vecs.h
+++ src/utils/maths/vecs.h
@@ -112,22 +112,22 @@ typedef union ivec4 {
 #endif
 	INT d[4];
 } ivec4;
-void ivec4s_set(ivec4 *vec, INT v1, INT v2, INT v3, INT v4);
-void ivec4v_set(ivec4 *vec, const ivec4 *vec2);
-void ivec4_set(ivec4 *vec, INT v[4]);
-void ivec4s_mul(ivec4 *vec, INT v1, INT v2, INT v3, INT v4);
-void ivec4_mul(ivec4 *vec, INT v[4]);
-void ivec4u_mul(ivec4 *vec, INT v);
-void ivec4v_mul(ivec4 *vec, const ivec4 *vec2);
-void ivec4s_add(ivec4 *vec, INT v1, INT v2, INT v3, INT v4);
-void ivec4_add(ivec4 *vec, INT v[4]);
-void ivec4u_add(ivec4 *vec, INT v);
-void ivec4v_add(ivec4 *vec, const ivec4 *vec2);
-void ivec4s_sub(ivec4 *vec, INT v1, INT v2, INT v3, INT v4);
-void ivec4_sub(ivec4 *vec, INT v[4]);
-void ivec4u_sub(ivec4 *vec, INT v);
-void ivec4v_sub(ivec4 *vec, const ivec4 *vec2);
-FLOAT ivec4_mag(const ivec4 *vec);
+void ivec4s_set(ivec4 *vec0, INT v0, INT v1, INT v2, INT v3);
+void ivec4v_set(ivec4 *vec0, const ivec4 *vec1);
+void ivec4_set(ivec4 *vec0, INT v1[4]);
+void ivec4s_mul(ivec4 *vec0, INT v0, INT v1, INT v2, INT v3);
+void ivec4_mul(ivec4 *vec0, INT v1[4]);
+void ivec4u_mul(ivec4 *vec0, INT v1);
+void ivec4v_mul(ivec4 *vec0, const ivec4 *vec1);
+void ivec4s_add(ivec4 *vec0, INT v0, INT v1, INT v2, INT v3);
+void ivec4_add(ivec4 *vec0, INT v1[4]);
+void ivec4u_add(ivec4 *vec0, INT v);
+void ivec4v_add(ivec4 *vec0, const ivec4 *vec1);
+void ivec4s_sub(ivec4 *vec0, INT v0, INT v1, INT v2, INT v3);
+void ivec4_sub(ivec4 *vec0, INT v1[4]);
+void ivec4u_sub(ivec4 *vec0, INT v1);
+void ivec4v_sub(ivec4 *vec0, const ivec4 *vec1);
+FLOAT ivec4_mag(const ivec4 *vec0);
 
 typedef union dvec2 {
 #if MATH_USE_STRUCTS
--- src/utils/maths/vecs/iv4.c
+++ src/utils/maths/vecs/iv4.c
@@ -3,131 +3,131 @@
 
 #include "../vecs.h"
 
-void ivec4s_set(ivec4 *vec, INT v1, INT v2, INT v3, INT v4)
+void ivec4s_set(ivec4 *vec0, INT v0, INT v1, INT v2, INT v3)
 {
-	assert(vec);
-	vec->d[0] = v1;
-	vec->d[1] = v2;
-	vec->d[2] = v3;
-	vec->d[3] = v4;
+	assert(vec0);
+	vec0->d[0] = v0;
+	vec0->d[1] = v1;
+	vec0->d[2] = v2;
+	vec0->d[3] = v3;
 }
-void ivec4v_set(ivec4 *vec, const ivec4 *vec2)
+void ivec4v_set(ivec4 *vec0, const ivec4 *vec1)
 {
-	assert(vec);
-	assert(vec2);
-	memcpy(vec, vec2, sizeof(*vec));
+	assert(vec0);
+	assert(vec1);
+	memcpy(vec0, vec1, sizeof(ivec4));
 }
-void ivec4_set(ivec4 *vec, INT v[4])
+void ivec4_set(ivec4 *vec0, INT v1[4])
 {
-	assert(vec);
-	memcpy(vec->d, v, 4 * sizeof(INT));
-}
-
-void ivec4s_mul(ivec4 *vec, INT v1, INT v2, INT v3, INT v4)
-{
-	assert(vec);
-	vec->d[0] *= v1;
-	vec->d[1] *= v2;
-	vec->d[2] *= v3;
-	vec->d[3] *= v4;
-}
-void ivec4_mul(ivec4 *vec, INT v[4])
-{
-	assert(vec);
-	vec->d[0] *= v[0];
-	vec->d[1] *= v[1];
-	vec->d[2] *= v[2];
-	vec->d[3] *= v[3];
-}
-void ivec4u_mul(ivec4 *vec, INT v)
-{
-	assert(vec);
-	vec->d[0] *= v;
-	vec->d[1] *= v;
-	vec->d[2] *= v;
-	vec->d[3] *= v;
-}
-void ivec4v_mul(ivec4 *vec, const ivec4 *vec2)
-{
-	assert(vec);
-	assert(vec2);
-	vec->d[0] *= vec2->d[0];
-	vec->d[1] *= vec2->d[1];
-	vec->d[2] *= vec2->d[2];
-	vec->d[3] *= vec2->d[3];
+	assert(vec0);
+	memcpy(vec0->d, v1, 4 * sizeof(INT));
 }
 
-void ivec4s_add(ivec4 *vec, INT v1, INT v2, INT v3, INT v4)
+void ivec4s_mul(ivec4 *vec0, INT v0, INT v1, INT v2, INT v3)
 {
-	assert(vec);
-	vec->d[0] += v1;
-	vec->d[1] += v2;
-	vec->d[2] += v3;
-	vec->d[3] += v4;
+	assert(vec0);
+	vec0->d[0] *= v0;
+	vec0->d[1] *= v1;
+	vec0->d[2] *= v2;
+	vec0->d[3] *= v3;
 }
-void ivec4_add(ivec4 *vec, INT v[4])
+void ivec4_mul(ivec4 *vec0, INT v1[4])
 {
-	assert(vec);
-	vec->d[0] += v[0];
-	vec->d[1] += v[1];
-	vec->d[2] += v[2];
-	vec->d[3] += v[3];
+	assert(vec0);
+	vec0->d[0] *= v1[0];
+	vec0->d[1] *= v1[1];
+	vec0->d[2] *= v1[2];
+	vec0->d[3] *= v1[3];
 }
-void ivec4u_add(ivec4 *vec, INT v)
+void ivec4u_mul(ivec4 *vec0, INT v1)
 {
-	assert(vec);
-	vec->d[0] += v;
-	vec->d[1] += v;
-	vec->d[2] += v;
-	vec->d[3] += v;
+	assert(vec0);
+	vec0->d[0] *= v1;
+	vec0->d[1] *= v1;
+	vec0->d[2] *= v1;
+	vec0->d[3] *= v1;
 }
-void ivec4v_add(ivec4 *vec, const ivec4 *vec2)
+void ivec4v_mul(ivec4 *vec0, const ivec4 *vec1)
 {
-	assert(vec);
-	assert(vec2);
-	vec->d[0] += vec2->d[0];
-	vec->d[1] += vec2->d[1];
-	vec->d[2] += vec2->d[2];
-	vec->d[3] += vec2->d[3];
-}
-
-void ivec4s_sub(ivec4 *vec, INT v1, INT v2, INT v3, INT v4)
-{
-	assert(vec);
-	vec->d[0] -= v1;
-	vec->d[1] -= v2;
-	vec->d[2] -= v3;
-	vec->d[3] -= v4;
-}
-void ivec4_sub(ivec4 *vec, INT v[4])
-{
-	assert(vec);
-	vec->d[0] -= v[0];
-	vec->d[1] -= v[1];
-	vec->d[2] -= v[2];
-	vec->d[3] -= v[3];
-}
-void ivec4u_sub(ivec4 *vec, INT v)
-{
-	assert(vec);
-	vec->d[0] -= v;
-	vec->d[1] -= v;
-	vec->d[2] -= v;
-	vec->d[3] -= v;
-}
-void ivec4v_sub(ivec4 *vec, const ivec4 *vec2)
-{
-	assert(vec);
-	assert(vec2);
-	vec->d[0] -= vec2->d[0];
-	vec->d[1] -= vec2->d[1];
-	vec->d[2] -= vec2->d[2];
-	vec->d[3] -= vec2->d[3];
+	assert(vec0);
+	assert(vec1);
+	vec0->d[0] *= vec1->d[0];
+	vec0->d[1] *= vec1->d[1];
+	vec0->d[2] *= vec1->d[2];
+	vec0->d[3] *= vec1->d[3];
 }
 
-FLOAT ivec4_mag(const ivec4 *vec)
+void ivec4s_add(ivec4 *vec0, INT v0, INT v1, INT v2, INT v3)
 {
-	return sqrt(pow(vec->d[0], 2) + pow(vec->d[1], 2) + pow(vec->d[2], 2) +
-		    pow(vec->d[3], 2));
+	assert(vec0);
+	vec0->d[0] += v0;
+	vec0->d[1] += v1;
+	vec0->d[2] += v2;
+	vec0->d[3] += v3;
+}
+void ivec4_add(ivec4 *vec0, INT v1[4])
+{
+	assert(vec0);
+	vec0->d[0] += v1[0];
+	vec0->d[1] += v1[1];
+	vec0->d[2] += v1[2];
+	vec0->d[3] += v1[3];
+}
+void ivec4u_add(ivec4 *vec0, INT v1)
+{
+	assert(vec0);
+	vec0->d[0] += v1;
+	vec0->d[1] += v1;
+	vec0->d[2] += v1;
+	vec0->d[3] += v1;
+}
+void ivec4v_add(ivec4 *vec0, const ivec4 *vec1)
+{
+	assert(vec0);
+	assert(vec1);
+	vec0->d[0] += vec1->d[0];
+	vec0->d[1] += vec1->d[1];
+	vec0->d[2] += vec1->d[2];
+	vec0->d[3] += vec1->d[3];
+}
+
+void ivec4s_sub(ivec4 *vec0, INT v0, INT v1, INT v2, INT v3)
+{
+	assert(vec0);
+	vec0->d[0] -= v0;
+	vec0->d[1] -= v1;
+	vec0->d[2] -= v2;
+	vec0->d[3] -= v3;
+}
+void ivec4_sub(ivec4 *vec0, INT v1[4])
+{
+	assert(vec0);
+	vec0->d[0] -= v1[0];
+	vec0->d[1] -= v1[1];
+	vec0->d[2] -= v1[2];
+	vec0->d[3] -= v1[3];
+}
+void ivec4u_sub(ivec4 *vec0, INT v1)
+{
+	assert(vec0);
+	vec0->d[0] -= v1;
+	vec0->d[1] -= v1;
+	vec0->d[2] -= v1;
+	vec0->d[3] -= v1;
+}
+void ivec4v_sub(ivec4 *vec0, const ivec4 *vec1)
+{
+	assert(vec0);
+	assert(vec1);
+	vec0->d[0] -= vec1->d[0];
+	vec0->d[1] -= vec1->d[1];
+	vec0->d[2] -= vec1->d[2];
+	vec0->d[3] -= vec1->d[3];
+}
+
+FLOAT ivec4_mag(const ivec4 *vec0)
+{
+	return sqrt(pow(vec0->d[0], 2) + pow(vec0->d[1], 2) +
+		    pow(vec0->d[2], 2) + pow(vec0->d[3], 2));
 }
 #endif
